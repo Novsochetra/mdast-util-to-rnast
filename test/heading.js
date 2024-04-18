612d8c4766c21008e11780da8a11a0e0c4e903d4
Novsochetra <sochetra12.nov@gmail.com>
2024-04-18 10:16:02 +0700

    fix(test): heading.js

--- test/heading.js
+++ test/heading.js
@@ -3,9 +3,13 @@ import assert from 'node:assert/strict'
 import {toRNast} from '../index.js'
 import {FONT_SIZE_MULTIPLIER} from '../elements/heading.js'
 
+const MAXIMUM_SIZE = 36
+
 test('heading element', async function (t) {
   await t.test(
-    'should transform `heading 1` to `<Text style={{fontSize: 30.5}} />`',
+    `should transform 'heading 1' to '<Text style={{fontSize: ${
+      MAXIMUM_SIZE - 1 * FONT_SIZE_MULTIPLIER
+    }}} />'`,
     async function () {
       const mdast = adastHeading(1)
 
@@ -17,7 +21,9 @@ test('heading element', async function (t) {
   )
 
   await t.test(
-    'should transform `heading 2` to `<Text style={{fontSize: 29}} />`',
+    `should transform 'heading 2' to '<Text style={{fontSize: ${
+      MAXIMUM_SIZE - 2 * FONT_SIZE_MULTIPLIER
+    }}} />'`,
     async function () {
       const mdast = adastHeading(2)
 
@@ -29,7 +35,9 @@ test('heading element', async function (t) {
   )
 
   await t.test(
-    'should transform `heading 3` to `<Text style={{fontSize: 27.5}} />`',
+    `should transform 'heading 3' to '<Text style={{fontSize: ${
+      MAXIMUM_SIZE - 3 * FONT_SIZE_MULTIPLIER
+    }}} />'`,
     async function () {
       const mdast = adastHeading(3)
 
@@ -41,7 +49,9 @@ test('heading element', async function (t) {
   )
 
   await t.test(
-    'should transform `heading 4` to `<Text style={{fontSize: 26}} />`',
+    `should transform 'heading 4' to '<Text style={{fontSize: ${
+      MAXIMUM_SIZE - 4 * FONT_SIZE_MULTIPLIER
+    }}} />'`,
     async function () {
       const mdast = adastHeading(4)
 
@@ -53,7 +63,9 @@ test('heading element', async function (t) {
   )
 
   await t.test(
-    'should transform `heading 5` to `<Text style={{fontSize: 24.5}} />`',
+    `should transform 'heading 5' to '<Text style={{fontSize: ${
+      MAXIMUM_SIZE - 5 * FONT_SIZE_MULTIPLIER
+    }}} />'`,
     async function () {
       const mdast = adastHeading(5)
 
@@ -65,7 +77,9 @@ test('heading element', async function (t) {
   )
 
   await t.test(
-    'should transform `heading 6` to `<Text style={{fontSize: 23}} />`',
+    `should transform 'heading 6' to '<Text style={{fontSize: ${
+      MAXIMUM_SIZE - 6 * FONT_SIZE_MULTIPLIER
+    }}} />'`,
     async function () {
       const mdast = adastHeading(6)
 
@@ -93,7 +107,7 @@ function expectedOutputRNast(type) {
     tagName: 'Text',
     properties: {
       style: {
-        fontSize: 32 - type * FONT_SIZE_MULTIPLIER,
+        fontSize: 36 - type * FONT_SIZE_MULTIPLIER,
         fontWeight: 'bold'
       }
     }
